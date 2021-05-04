450dbc1d3ccd1927d8c0903a4509e39ac4d80fc9
Georgi Naumov <gonaumov@gmail.com>
2021-05-04 23:30:36 +0300

    Deployment

---
+++ docs/precache-manifest.7f3f8dfe2080a04bd1caa2143f154b83.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55370849cec2b953d30e",
+    "url": "/internationalrecipes/css/app.1356acd6.css"
+  },
+  {
+    "revision": "0ec7ec45a092b4db31c0",
+    "url": "/internationalrecipes/css/chunk-vendors.8d159d84.css"
+  },
+  {
+    "revision": "17a00426e04d5cb9113f5968dcd0afe4",
+    "url": "/internationalrecipes/fonts/element-icons.17a00426.woff"
+  },
+  {
+    "revision": "8e2d08c4c643dcfbf6ec4f185c884d8a",
+    "url": "/internationalrecipes/fonts/element-icons.8e2d08c4.ttf"
+  },
+  {
+    "revision": "7b943fccdbce0087edab7630fe38e078",
+    "url": "/internationalrecipes/img/banica.7b943fcc.png"
+  },
+  {
+    "revision": "730e885fae96556fcbe1fbbaafd9c7f2",
+    "url": "/internationalrecipes/img/banicharnica.730e885f.png"
+  },
+  {
+    "revision": "3644f010105d427230148c3366944723",
+    "url": "/internationalrecipes/img/khichdi.3644f010.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/internationalrecipes/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "236ff55103e3ca2b857866b979376de0",
+    "url": "/internationalrecipes/img/pumpkinRisottowithChorizo.236ff551.png"
+  },
+  {
+    "revision": "b2e81a92cfc61b8e1597d3abf7d8a6b3",
+    "url": "/internationalrecipes/img/pumpkinRisottowithChorizo1.b2e81a92.png"
+  },
+  {
+    "revision": "89d019182c361475e5ecefbc8aca0218",
+    "url": "/internationalrecipes/img/pumpkinRisottowithChorizo2.89d01918.png"
+  },
+  {
+    "revision": "7c8664b3f0142f0e6ba889f6aa88aec9",
+    "url": "/internationalrecipes/img/pumpkinRisottowithChorizo3.7c8664b3.png"
+  },
+  {
+    "revision": "f1a0a4426e766cbca6ad073d5ad88efd",
+    "url": "/internationalrecipes/img/pumpkinRisottowithChorizo4.f1a0a442.png"
+  },
+  {
+    "revision": "55b63eac2855b007563daeeb7edfa878",
+    "url": "/internationalrecipes/img/pumpkinRisottowithChorizo5.55b63eac.png"
+  },
+  {
+    "revision": "17f00f97874b95e66da52b069bbf3476",
+    "url": "/internationalrecipes/img/strawberry-duplings-breadcrumbs.17f00f97.png"
+  },
+  {
+    "revision": "793f984b66422af940bd77ff0188f4a6",
+    "url": "/internationalrecipes/img/strawberry-duplings.793f984b.png"
+  },
+  {
+    "revision": "1939aed8ba77b0937f159d58c3f650a1",
+    "url": "/internationalrecipes/index.html"
+  },
+  {
+    "revision": "ace79ff1a784055f3c3c",
+    "url": "/internationalrecipes/js/about.67423b34.js"
+  },
+  {
+    "revision": "55370849cec2b953d30e",
+    "url": "/internationalrecipes/js/app.63f557f8.js"
+  },
+  {
+    "revision": "0ec7ec45a092b4db31c0",
+    "url": "/internationalrecipes/js/chunk-vendors.c4e572ae.js"
+  },
+  {
+    "revision": "63d4571d82de88b2c98ce9626ba38867",
+    "url": "/internationalrecipes/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/internationalrecipes/robots.txt"
+  }
+]);
